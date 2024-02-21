b8694a1593542539400eb7ab2a7133b2061c64be
Zachary Lim <zacthinks@outlook.com>
2024-02-21 11:06:18 -0500

    Initialize project
    - Add README.md
    - Add .gitignore (ignore data folder)
    - Add first script to generate pipeline image and its requirements

---
+++ README.md
@@ -0,0 +1,1 @@
+# Semantic Annotations Pipeline
---
+++ draw_pipeline.py
@@ -0,0 +1,11 @@
+import schemdraw
+from schemdraw import flow
+
+
+def main():
+    with schemdraw.Drawing(file='pipeline.svg') as d:
+        pass
+
+
+if __name__ == '__main__':
+    main()
