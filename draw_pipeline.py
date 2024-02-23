4d91970367902017c7991f43c409a6f4e37f2dbe
Zachary Lim <zacthinks@outlook.com>
2024-02-23 14:34:34 -0500

    Clean draw_pipeline.py code

--- draw_pipeline.py
+++ draw_pipeline.py
@@ -5,30 +5,31 @@ from schemdraw import flow
 
 def main():
     with schemdraw.Drawing(file="pipeline.svg") as d:
+        label_fontsize = 10
+        arrow_length = d.unit * 2 / 3
+
         flow.Data().label("Constellate")
-        flow.Arrow().right(d.unit * 2 / 3).label("cleaner.py", fontsize=10)
+        flow.Arrow().right(arrow_length).label("cleaner.py", fontsize=label_fontsize)
         flow.Box().label("Cleaned docs")
-        flow.Arrow().right(d.unit * 2 / 3).label("subsetter.py", fontsize=10)
+        flow.Arrow().right(arrow_length).label("subsetter.py", fontsize=label_fontsize)
         flow.Box().label("Subsetted docs")
-        flow.Arrow().right(d.unit * 2 / 3).label("sentencizer.py", fontsize=10)
+        flow.Arrow().right(arrow_length).label("sentencizer.py", fontsize=label_fontsize)
         s = flow.Box().label("Sentences")
 
-        elm.Wire("|-", arrow="->").at(s.N).to((s.N[0] + d.unit * 2 / 3, s.N[1] + d.unit / 3)).label("srl.py", fontsize=10)
+        elm.Wire("|-", arrow="->").at(s.N).to((s.N[0] + arrow_length, s.N[1] + d.unit / 3)).label("srl.py", fontsize=label_fontsize)
         pn = flow.Box().label("Protonarratives")
 
-        elm.Wire("|-", arrow="->").at(s.S).to((s.N[0] + d.unit * 2 / 3 + .28431, s.S[1] - d.unit / 3)).label("dp.py", fontsize=10)
+        elm.Wire("|-", arrow="->").at(s.S).to((s.N[0] + arrow_length + .28431, s.S[1] - d.unit / 3)).label("dp.py", fontsize=label_fontsize)
         dp = flow.Box().label("Dependency\nparses")
 
         joint_loc = (dp.E[0] + d.unit / 3, dp.E[1] + d.unit / 3 + 1)
         elm.Wire("-|").at(dp.E).to(joint_loc)
         elm.Wire("-|").at(pn.E).to(joint_loc)
 
-        flow.Arrow().right(d.unit * 2 / 3).label("annotator.py", fontsize=10)
+        flow.Arrow().right(arrow_length).label("annotator.py", fontsize=label_fontsize)
         flow.Box().label("Annotated\nprotonarratives")
-        flow.Arrow().right(d.unit * 2 / 3).label("clusterer.py", fontsize=10)
+        flow.Arrow().right(arrow_length).label("clusterer.py", fontsize=label_fontsize)
         flow.Box().label("Protonarrative\nclusters")
-
-        dp.at(pn.S)
 
 
 if __name__ == '__main__':
---
+++ src/utils/page_merger.py
@@ -0,0 +1,93 @@
+import re
+
+def get_max_match(text, loc, delim, window=100, threshold=3):
+    left = text[max(0,loc-window):loc]
+    right = text[loc+len(delim):loc+len(delim)+window]
+    left_str = r"\s*"
+    right_str = r"\s*"
+    match_count = 0
+    
+    while True:
+        next_block = re.search(r"(\S+)" + left_str + "$", left).group(1)
+        if next_block.isdigit():
+            new_left_str = r"\s*\d+" + left_str
+        else:
+            new_left_str = r"\s*" + re.escape(next_block) + left_str
+            
+        new_match_count = len(re.findall(new_left_str + delim, text, re.IGNORECASE))
+        if new_match_count >= threshold:
+            left_str = new_left_str
+            match_count = new_match_count
+        else:
+            break
+    
+    while True:
+        next_block = re.search("^" + right_str + r"(\S+)", right).group(1)
+        if next_block.isdigit():
+            new_right_str = right_str + r"\d+\s*"
+        else:
+            new_right_str = right_str + re.escape(next_block) + r"\s*" 
+            
+        new_match_count = len(re.findall(left_str + delim + new_right_str, text, re.IGNORECASE))
+        if new_match_count >= threshold:
+            right_str = new_right_str
+            match_count = new_match_count
+        else:
+            break
+
+    if match_count > 0:
+        return re.compile(left_str + delim + right_str, re.IGNORECASE), match_count
+    else:
+        return None
+
+def crude_headerfooter_matcher(text, loc, delim, window=100, min_len=3):
+    d_len = len(delim)
+    left = text[loc-window:loc]
+    right = text[loc+d_len:loc+d_len+window]
+    left_len = len(re.search(r"[^a-z]*$", left).group())
+    right_len = len(re.search(r"^[^a-z]*", right).group())
+
+    if left_len < min_len:
+        left_len = 0
+    if right_len < min_len:
+        right_len = 0
+    if left_len + right_len > 0:
+        return loc - left_len, loc + d_len + right_len
+    else:
+        return None
+
+def join_pages(pages, 
+               delim = "@@@PAGE@@@", 
+               delim_regex = re.compile('@@@PAGE@@@', re.IGNORECASE),
+               use_crude=True,
+               verbose=False):
+    def repl_func(matchobj):
+        nonlocal first
+        if verbose:
+            print("\tDeleted by get_max_match: ", matchobj.group())
+        if first:
+            return " "
+            first = False
+        else:
+            return delim
+            
+    joined = delim.join(pages)
+    n_delim = len(pages) - 1
+    while len(delim_regex.findall(joined)) > n_delim:
+        delim = f"@{delim}@"
+        delim_regex = re.compile(delim)
+        joined = delim.join(pages)
+        
+    while match := delim_regex.search(joined):
+        loc = match.start()
+        first = True
+        if max_match := get_max_match(joined, loc, delim):
+            if verbose: print(f"{max_match[1]} matches ({max_match[1] / n_delim:.2%}) found by get_max_match to be deleted:")
+            joined = max_match[0].sub(repl_func, joined)
+        elif use_crude and (crude_match := crude_headerfooter_matcher(joined, loc, delim)):
+            if verbose: print("Crude match to be deleted: ", joined[crude_match[0]:crude_match[1]])
+            joined = joined[:crude_match[0]] + " " + joined[crude_match[1]:]
+        else:
+            joined = joined[:loc] + " " + joined[loc + len(delim):]
+
+    return joined
