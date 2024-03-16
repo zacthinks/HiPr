eeeb004806adc5b829a3e3fbb8bc16ebb9fa465f
Zachary Lim <zacthinks@outlook.com>
2024-03-16 12:54:07 -0400

    Add page_merger.py
    Add page merger.py so cleaner.py can use it. Also made a minor edit to .gitignore to ignore jupyter files.

--- src/utils/page_merger.py
+++ src/utils/page_merger.py
@@ -1,33 +1,34 @@
 import re
 
+
 def get_max_match(text, loc, delim, window=100, threshold=3):
-    left = text[max(0,loc-window):loc]
-    right = text[loc+len(delim):loc+len(delim)+window]
+    left = text[max(0, loc - window):loc]
+    right = text[loc + len(delim):loc + len(delim) + window]
     left_str = r"\s*"
     right_str = r"\s*"
     match_count = 0
-    
+
     while True:
         next_block = re.search(r"(\S+)" + left_str + "$", left).group(1)
         if next_block.isdigit():
             new_left_str = r"\s*\d+" + left_str
         else:
             new_left_str = r"\s*" + re.escape(next_block) + left_str
-            
+
         new_match_count = len(re.findall(new_left_str + delim, text, re.IGNORECASE))
         if new_match_count >= threshold:
             left_str = new_left_str
             match_count = new_match_count
         else:
             break
-    
+
     while True:
         next_block = re.search("^" + right_str + r"(\S+)", right).group(1)
         if next_block.isdigit():
             new_right_str = right_str + r"\d+\s*"
         else:
-            new_right_str = right_str + re.escape(next_block) + r"\s*" 
-            
+            new_right_str = right_str + re.escape(next_block) + r"\s*"
+
         new_match_count = len(re.findall(left_str + delim + new_right_str, text, re.IGNORECASE))
         if new_match_count >= threshold:
             right_str = new_right_str
@@ -40,10 +41,11 @@ def get_max_match(text, loc, delim, window=100, threshold=3):
     else:
         return None
 
+
 def crude_headerfooter_matcher(text, loc, delim, window=100, min_len=3):
     d_len = len(delim)
-    left = text[loc-window:loc]
-    right = text[loc+d_len:loc+d_len+window]
+    left = text[loc - window:loc]
+    right = text[loc + d_len:loc + d_len + window]
     left_len = len(re.search(r"[^a-z]*$", left).group())
     right_len = len(re.search(r"^[^a-z]*", right).group())
 
@@ -56,9 +58,10 @@ def crude_headerfooter_matcher(text, loc, delim, window=100, min_len=3):
     else:
         return None
 
-def join_pages(pages, 
-               delim = "@@@PAGE@@@", 
-               delim_regex = re.compile('@@@PAGE@@@', re.IGNORECASE),
+
+def join_pages(pages,
+               delim="@@@PAGE@@@",
+               delim_regex=re.compile('@@@PAGE@@@', re.IGNORECASE),
                use_crude=True,
                verbose=False):
     def repl_func(matchobj):
@@ -70,22 +73,24 @@ def join_pages(pages,
             first = False
         else:
             return delim
-            
+
     joined = delim.join(pages)
     n_delim = len(pages) - 1
     while len(delim_regex.findall(joined)) > n_delim:
         delim = f"@{delim}@"
         delim_regex = re.compile(delim)
         joined = delim.join(pages)
-        
+
     while match := delim_regex.search(joined):
         loc = match.start()
         first = True
         if max_match := get_max_match(joined, loc, delim):
-            if verbose: print(f"{max_match[1]} matches ({max_match[1] / n_delim:.2%}) found by get_max_match to be deleted:")
+            if verbose:
+                print(f"{max_match[1]} matches ({max_match[1] / n_delim:.2%}) found by get_max_match to be deleted:")
             joined = max_match[0].sub(repl_func, joined)
         elif use_crude and (crude_match := crude_headerfooter_matcher(joined, loc, delim)):
-            if verbose: print("Crude match to be deleted: ", joined[crude_match[0]:crude_match[1]])
+            if verbose:
+                print("Crude match to be deleted: ", joined[crude_match[0]:crude_match[1]])
             joined = joined[:crude_match[0]] + " " + joined[crude_match[1]:]
         else:
             joined = joined[:loc] + " " + joined[loc + len(delim):]
